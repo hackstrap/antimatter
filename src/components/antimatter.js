70c8701fc93f545d83a45afe985f76b844fca208
anubhav201241 <201241@juitsolan.in>
2021-09-28 21:15:28 +0530

    Added Frontend Files

---
+++ src/components/antimatter.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import './antimatter.css';
+import { BiHome } from "react-icons/bi";
+import { BiCog } from "react-icons/bi";
+import { BiGrid } from "react-icons/bi";
+const Antimatter = () => {
+    return (
+        <>
+            <section>
+                <div class="main_icon">
+                    <div class="icon1 icon_div">
+                        <span class="icon"><BiHome /></span>
+                    </div>
+                        
+                    <div class="icon2 icon_div">
+                       <span class="icon"><BiGrid /></span>
+                    </div>
+
+                    <div class="icon3 icon_div">
+                       <span class="icon"><BiCog /></span>
+                    </div>
+                </div>
+
+                <h1>Welcome to Antimatter</h1>
+                <p>Set Up Your ELT and Analytical infrastructure</p>
+                <div class="cards">
+                    <div className="card">
+                        <div className="main1">
+                        </div>
+                    </div>
+
+
+                    <div className="card">
+                        <div className="main2">
+                        </div>
+                    </div>
+                </div>
+                <div class="main3">
+                    <p class="jitsu">Centralize data with Jitsu</p>
+                    <button type = 'submit' class="btn">Start Now</button>
+                </div>
+                <div class="main4">
+                    <p class="click">Connect to your Data Warehouse</p>
+                    <button type = 'submit' class="butn">Start Now</button>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Antimatter
