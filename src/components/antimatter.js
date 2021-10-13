ea8467b72a76cbb6a0233216eecd86fa2ee2b951
anubhav201241 <201241@juitsolan.in>
2021-10-14 02:38:12 +0530

    Added links

--- src/components/antimatter.js
+++ src/components/antimatter.js
@@ -23,21 +23,37 @@ const Antimatter = () => {
 
                 <h1>Welcome to Antimatter</h1>
                 <p>Set Up Your ELT and Analytical infrastructure</p>
-                <div class="cards">
+                <div class="jitsue">
+                        <p class="about"><span class="dot">1</span>Centralize data with Jitsu</p>
                         <div className="logo logo-jitsu"></div>
+                        <button type = 'submit' class="btn"><a href="http://localhost:8000/configurator" target="_blank">Start Now</a></button>
+                </div>
+                <div class="clickHousee">
+                        <p class="about"><span class="dot">2</span>Connect to your Data Warehouse</p>
                         <div className="logo logo-clickHouse"></div>
+                        <button type = 'submit' class="btn"><a href="http://localhost:8123/play" target="_blank">Start Now</a></button>
+                </div>
+                <div class="SQLPad">
+                        <p class="about"><span class="dot">3</span>Run Queries with Web-based SQL Editor</p>
+                        <div className="logo logo-SQLPad"></div>
+                        <button type = 'submit' class="btn"><a href="http://localhost:3002/" target="_blank">Start Now</a></button>
+                </div>
+                <div class="Trino">
+                        <p class="about"><span class="dot">4</span>Connect to your Data Warehouse</p>
+                        <div className="logo logo-Trino"></div>
+                        <button type = 'submit' class="btn"><a href="http://localhost:8080/ui/login.html" target="_blank">Start Now</a></button>
+                </div>
+                <div class="Metabase">
+                        <p class="about"><span class="dot">5</span>Connect to your Data Warehouse</p>
+                        <div className="logo logo-Metabase"></div>
+                        <button type = 'submit' class="btn"><a href="http://localhost:3001/" target="_blank">Start Now</a></button>
+                </div>
+                <div class="Cubejs">
+                        <p class="about"><span class="dot">6</span>Connect to your Data Warehouse</p>
+                        <div className="logo logo-Cubejs"></div>
+                        <button type = 'submit' class="btn"><a href="http://localhost:4000/#/connection" target="_blank">Start Now</a></button>
                 </div>
 
-                <div className="btn-group">
-                <div class="btn-box">
-                    <p class="about">Centralize data with Jitsu</p>
-                    <button type = 'submit' class="btn"><a href="https://jitsu.com/" target="_blank">Start Now</a></button>
-                </div>
-                <div class="btn-box">
-                    <p class="about">Connect to your Data Warehouse</p>
-                    <button type = 'submit' class="btn"><a href="https://clickhouse.com/" target="_blank">Start Now</a></button>
-                </div>
-                </div>
             </section>
         </>
     )
