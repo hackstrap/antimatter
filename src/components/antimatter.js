48703b742f9701dcc90229600a6eb15071ebb5dd
sandeep-devarapalli <sandeep.devarapalli@live.com>
2021-10-26 21:31:30 +0530

    modified:   docker-compose.yml         modified:   package-lock.json         modified:   public/index.html         modified:   src/components/antimatter.css         modified:   src/components/antimatter.js         new file:   src/svg/Jupyter.svg         new file:   src/svg/Tabix.png

--- src/components/antimatter.js
+++ src/components/antimatter.js
@@ -1,8 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { BiCog, BiGrid, BiHome } from "react-icons/bi";
 import './antimatter.css';
-import { BiHome } from "react-icons/bi";
-import { BiCog } from "react-icons/bi";
-import { BiGrid } from "react-icons/bi";
 const Antimatter = () => {
     return (
         <>
@@ -22,7 +20,8 @@ const Antimatter = () => {
                 </div>
 
                 <h1>Welcome to Antimatter</h1>
-                <p>Set Up Your ELT and Analytical infrastructure</p>
+                <p>Set Up Your ELT and Analytics Infrastructure</p>
+                <div class="application">
                 <div class="jitsue">
                         <p class="about"><span class="dot">1</span>Centralize data with Jitsu</p>
                         <div className="logo logo-jitsu"></div>
@@ -33,25 +32,36 @@ const Antimatter = () => {
                         <div className="logo logo-clickHouse"></div>
                         <button type = 'submit' class="btn"><a href="http://localhost:8123/play" target="_blank">Start Now</a></button>
                 </div>
+                <div class="Tabix">
+                        <p class="about"><span class="dot">3</span>Run Queries with Web-based ClickHouse Editor</p>
+                        <div className="logo logo-Tabix"></div>
+                        <button type = 'submit' class="btn"><a href="http://localhost:8081/" target="_blank">Start Now</a></button>
+                </div>
                 <div class="SQLPad">
-                        <p class="about"><span class="dot">3</span>Run Queries with Web-based SQL Editor</p>
+                        <p class="about"><span class="dot">4</span>Run Queries with Web-based SQL Editor</p>
                         <div className="logo logo-SQLPad"></div>
                         <button type = 'submit' class="btn"><a href="http://localhost:3002/" target="_blank">Start Now</a></button>
                 </div>
                 <div class="Trino">
-                        <p class="about"><span class="dot">4</span>Connect to your Data Warehouse</p>
+                        <p class="about"><span class="dot">5</span>Run queries with Fast distributed SQL query engine</p>
                         <div className="logo logo-Trino"></div>
                         <button type = 'submit' class="btn"><a href="http://localhost:8080/ui/login.html" target="_blank">Start Now</a></button>
                 </div>
+                <div class="Jupyter">
+                        <p class="about"><span class="dot">6</span>Open your Jupyter Data Science Notebook</p>
+                        <div className="logo logo-Jupyter"></div>
+                        <button type = 'submit' class="btn"><a href="http://localhost:8888/ui/login.html" target="_blank">Start Now</a></button>
+                </div>
                 <div class="Metabase">
-                        <p class="about"><span class="dot">5</span>Connect to your Data Warehouse</p>
+                        <p class="about"><span class="dot">7</span>Connect with your Business Intelligence tool</p>
                         <div className="logo logo-Metabase"></div>
                         <button type = 'submit' class="btn"><a href="http://localhost:3001/" target="_blank">Start Now</a></button>
                 </div>
                 <div class="Cubejs">
-                        <p class="about"><span class="dot">6</span>Connect to your Data Warehouse</p>
+                        <p class="about"><span class="dot">8</span>Setup an API layer to build data applications faster</p>
                         <div className="logo logo-Cubejs"></div>
                         <button type = 'submit' class="btn"><a href="http://localhost:4000/#/connection" target="_blank">Start Now</a></button>
+                </div>
                 </div>
 
             </section>
